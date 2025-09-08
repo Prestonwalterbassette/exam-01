481f37510b94859f45794384be6fdcaf5ae8ee89
Prestonwalterbassette <preston.walterbassette@ynov.com>
2025-09-08 09:57:24 +0200

    exercise1

--- exercise 1.go
+++ exercise 1.go
@@ -1,11 +1,16 @@
 package main
 
-func Surprise(s string) bool { if len(s) < 1 {         
-
-	return false  
-}
+func Surprise(s string) bool { 
 	
+	if len(s) < 1 {         
+		return false  
+}
+
 	return surprise_annexe(s[0]) || Surprise(s[1:])  
 
-}  func surprise_annexe(a byte) bool { return (a <= 'z' && a >= 'a') || (a <= 'Z' && a >= 'A') } 
+}  
+
+func surprise_annexe(a byte) bool { 
+	
+	return (a <= 'z' && a >= 'a') || (a <= 'Z' && a >= 'A') } 
 
